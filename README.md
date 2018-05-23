01457dd2ff2f23d3b35142085b8e98c45b6f3a13
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-05-23 15:12:31 +0700

    Add test SharedPreference between apps with same key

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 #### View SharedPreferences
 
 `File Explorer -> your package name -> shared_prefs`
+
+
+#### SharedPreferences between apps
+SharePreferences is different between apps even it is same key (I 
+have tested on 2 flavor of same app use same key)
--- app/src/main/java/toong/vn/androidsharedpreferences/MainActivity.java
+++ app/src/main/java/toong/vn/androidsharedpreferences/MainActivity.java
@@ -1,19 +1,37 @@
 package toong.vn.androidsharedpreferences;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 import toong.vn.androidsharedpreferences.api.SharedPreferenceApi;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvStatus = findViewById(R.id.text_status);
 
-        SharedPreferenceApi sharedPreferenceApi = new SharedPreferenceApi(this);
-        sharedPreferenceApi.put(SharedPreferenceApi.PREFS_USER_NAME, "PhanVanLinh");
+        final SharedPreferenceApi sharedPreferenceApi = new SharedPreferenceApi(this);
         sharedPreferenceApi.put(SharedPreferenceApi.PREFS_PASS_WORD, "Password");
         sharedPreferenceApi.put(SharedPreferenceApi.PREFS_PASS_WORD, null);
+
+        findViewById(R.id.button_put).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPreferenceApi.put(SharedPreferenceApi.PREFS_USER_NAME, "PhanVanLinh");
+                tvStatus.setText("Success");
+            }
+        });
+
+        findViewById(R.id.button_get).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvStatus.setText(sharedPreferenceApi.get(SharedPreferenceApi.PREFS_USER_NAME, String.class));
+            }
+        });
     }
 }
